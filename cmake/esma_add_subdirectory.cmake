3bd405dd781e28b0ef8d885722ce7892003d0e3b
Mark A Potts <Mark.Potts@noaa.gov>
2020-06-15 17:29:29 +0000

    updated

--- cmake/esma_add_subdirectory.cmake
+++
@@ -1,6 +0,0 @@
-macro (esma_add_subdirectory subdirectory)
-  if (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${subdirectory})
-    add_subdirectory (${subdirectory})
-  endif ()
-endmacro (esma_add_subdirectory)
-
