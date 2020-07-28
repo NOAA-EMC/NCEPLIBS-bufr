77ba6c40b92cc655760225e8fd8d5ad75a649658
Ron McLaren <ronlaren@aol.com>
2020-07-28 14:37:51 -0400

    now getting subset strings back from the readmg call. Other small name changes.

--- src/bufr.interface.h
+++ src/bufr.interface.h
@@ -8,11 +8,11 @@ extern "C" {
   void close_f(int unit);
   void openbf_f(int bufr_unit, const char* cio, int table_file_d);
   void closbf_f(int bufr_unit);
-  int ireadmg_f(int bufr_unit, char* subset, int* iddate);
+  int ireadmg_f(int bufr_unit, char* subset, int* iddate, int subset_len);
   int ireadsb_f(int bufr_unit);
   void ufbint_f(int bufr_unit, void** c_data, int dim_1, int dim_2, int* iret, const char* table_b_mnemonic);
   void ufbrep_f(int bufr_unit, void** c_data, int dim_1, int dim_2, int* iret, const char* table_b_mnemonic);
-  void dxdump_f(int bufr_unit, int output_unit);
+  void dxdump_f(int bufr_unit, int table_unit);
 
 #ifdef __cplusplus
 }
