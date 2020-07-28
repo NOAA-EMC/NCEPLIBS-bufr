06c0305db46c7c35e974021e1df0b1771ddd22fe
Ron McLaren <ronlaren@aol.com>
2020-07-28 10:26:48 -0400

    Updated bufr.interface code to fix Rahul's code review comments. Updated CMake files so that the bufr.interface is included as part of the NCEPLIB-bufr library, same as everything else.

--- src/bufr.interface.f90
+++ src/bufr.interface.f90
@@ -1,12 +1,20 @@
  
-module bufrlib_c_wrapper
+module bufr_c_interface_mod
   use iso_c_binding
+  implicit none
+
+  private
+  public :: open_c
+  public :: close_c
+  public :: openbf_c
+  public :: ireadmg_c
+  public :: ireadsb_c
+  public :: ufbint_c
+  public :: ufbrep_c
 
 contains
 
   function c_to_f_string(c_str) result(str)
-    implicit none
-
     character(kind=c_char,len=1), intent(in) :: c_str(*)
     character(len=:), allocatable :: str
     integer nchars
@@ -39,61 +47,51 @@ contains
   ! end function
 
 
-  subroutine open_c(unit, filepath) bind(C, name='open_f90')
-    implicit none
-
+  subroutine open_c(unit, filepath) bind(C, name='open_f')
     integer(c_int), value, intent(in) :: unit
     character(kind=c_char, len=1) :: filepath
 
-    open(unit, FILE=c_to_f_string(filepath))
+    open(unit, file=c_to_f_string(filepath))
   end subroutine open_c
 
 
-  subroutine close_c(unit) bind(C, name='close_f90')
-    implicit none
-
+  subroutine close_c(unit) bind(C, name='close_f')
     integer(c_int), value, intent(in) :: unit
 
-    close(UNIT=unit)
-  end SUBROUTINE close_c
+    close(unit=unit)
+  end subroutine close_c
 
 
-  subroutine openbf_c(bufr_unit, cio, table_file_id) bind(C, name='openbf_f90') 
-    implicit none
-
+  subroutine openbf_c(bufr_unit, cio, table_file_id) bind(C, name='openbf_f') 
     integer(c_int), value, intent(in) :: bufr_unit
     character(kind=c_char, len=1), intent(in) :: cio
     integer(c_int), value, intent(in) :: table_file_id
 
-    call OPENBF(bufr_unit, c_to_f_string(cio), table_file_id)
+    call openbf(bufr_unit, c_to_f_string(cio), table_file_id)
 
   end subroutine openbf_c
 
 
-  subroutine closbf_c(bufr_unit) bind(C, name='closbf_f90') 
-    implicit none
-
+  subroutine closbf_c(bufr_unit) bind(C, name='closbf_f') 
     integer(c_int), value, intent(in) :: bufr_unit
 
-    call CLOSBF(bufr_unit)
+    call closbf(bufr_unit)
 
   end subroutine closbf_c
 
 
-  function ireadmg_c(bufr_unit, subset, iddate) result(ires) bind(C, name='ireadmg_f90')
-    implicit none
-
+  function ireadmg_c(bufr_unit, subset, iddate) result(ires) bind(C, name='ireadmg_f')
     integer(c_int), value, intent(in) :: bufr_unit
     type(c_ptr), intent(out) :: subset
     integer(c_int), intent(out) :: iddate
     
     integer(c_int) :: ires
     integer :: subset_len
-    integer :: IREADMG
+    integer :: ireadmg
 
     character(len=25), pointer :: subset_ptr
     character(len=25), target :: f_subset
-    ires = IREADMG(bufr_unit, f_subset, iddate)
+    ires = ireadmg(bufr_unit, f_subset, iddate)
 
     ! !convert fortran string to c string
     ! subset_ptr => f_subset
@@ -104,22 +102,18 @@ contains
   end function ireadmg_c
 
 
-  function ireadsb_c(bufr_unit) result(ires) bind(C, name='ireadsb_f90')
-    implicit none
-
+  function ireadsb_c(bufr_unit) result(ires) bind(C, name='ireadsb_f')
     integer(c_int), value, intent(in) :: bufr_unit
 
     integer(c_int) :: ires
-    integer :: IREADSB
+    integer :: ireadsb
     
-    ires = IREADSB(bufr_unit)
+    ires = ireadsb(bufr_unit)
 
   end function ireadsb_c
 
 
-  subroutine ufbint_c(bufr_unit, c_data, dim_1, dim_2, iret, table_b_mnemonic) bind(C, name='ufbint_f90')
-    implicit none
-
+  subroutine ufbint_c(bufr_unit, c_data, dim_1, dim_2, iret, table_b_mnemonic) bind(C, name='ufbint_f')
     integer(c_int), value, intent(in) :: bufr_unit
     type(c_ptr), intent(inout) ::  c_data
     integer(c_int), value, intent(in) :: dim_1, dim_2
@@ -129,14 +123,12 @@ contains
     real, pointer :: f_data
     call c_f_pointer(c_data, f_data)
     
-    call UFBINT(bufr_unit, f_data, dim_1, dim_2, iret, c_to_f_string(table_b_mnemonic))
+    call ufbint(bufr_unit, f_data, dim_1, dim_2, iret, c_to_f_string(table_b_mnemonic))
 
   end subroutine ufbint_c
 
 
-  subroutine ufbrep_c(bufr_unit, c_data, dim_1, dim_2, iret, table_b_mnemonic) bind(C, name='ufbrep_f90')
-    implicit none
-
+  subroutine ufbrep_c(bufr_unit, c_data, dim_1, dim_2, iret, table_b_mnemonic) bind(C, name='ufbrep_f')
     integer(c_int), value, intent(in) :: bufr_unit
     type(c_ptr), intent(inout) :: c_data
     integer(c_int), value, intent(in) :: dim_1, dim_2
@@ -146,8 +138,8 @@ contains
     real, pointer :: f_data
     call c_f_pointer(c_data, f_data)
 
-    call UFBREP(bufr_unit, f_data, dim_1, dim_2, iret, c_to_f_string(table_b_mnemonic))
+    call ufbrep(bufr_unit, f_data, dim_1, dim_2, iret, c_to_f_string(table_b_mnemonic))
 
   end subroutine ufbrep_c
 
-end module
+end module bufr_c_interface_mod
