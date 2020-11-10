8144920bbfd2459708b686bbf939da5d350a3299
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-11-10 14:48:28 -0500

    add python test

--- python/test/test_checkpoint.py
+++ python/test/test_checkpoint.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import sys
 import ncepbufr
 
 hdstr='SID XOB YOB DHR TYP ELV SAID T29'
@@ -9,7 +10,7 @@ oestr='POE QOE TOE NUL WOE NUL PWE     '
 # read prepbufr file.
 
 print('the following lines should be the same')
-bufr = ncepbufr.open('data/prepbufr')
+bufr = ncepbufr.open(sys.argv[1])
 nmsg = 0
 while bufr.advance() == 0:
     #print(nmsg, bufr.msg_counter, bufr.msg_type, bufr.msg_date)
