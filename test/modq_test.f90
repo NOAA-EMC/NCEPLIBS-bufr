f09e3dfc5129a766ca03cfb1cef8e95276a563ee
Ron McLaren <ronald.mclaren@noaa.gov>
2021-07-09 15:05:37 -0400

    Improved modq test class.

--- test/modq_test.f90
+++ test/modq_test.f90
@@ -1,72 +1,124 @@
 
 module modq_test
+  use modq_string
   implicit none
 
-  interface compare_arrays
-    module procedure :: compare_arrays_r_8_4
-    module procedure :: compare_arrays_r_4_4
-    module procedure :: compare_arrays_r_4_8
-    module procedure :: compare_arrays_r_8_8
-  end interface compare_arrays
+  type, public :: TestInstance
+    private
+      type(String) :: test_name
+
+    contains
+      procedure :: failed => test__failed
+      procedure :: compare_arrays => test__compare_arrays
+  end type TestInstance
+
+  interface TestInstance
+    module procedure initialize__test
+  end interface TestInstance
 
   private
-  public :: compare_arrays
-  public :: reset_file
+  public::reset_file
 
 contains
 
-  subroutine compare_arrays_r_8_4(array1, array2)
+  type(TestInstance) function initialize__test(name) result(test)
+    character(len=*) :: name
+
+    test = TestInstance(String(name))
+  end function initialize__test
+
+  subroutine test__failed(self, msg)
+    class(TestInstance), intent(in) :: self
+    character(len=*) :: msg
+
+    character(len=:), allocatable :: abort_msg
+
+    abort_msg = self%test_name%chars() // ": " // msg
+    call bort(abort_msg)
+  end subroutine
+
+  subroutine test__compare_arrays(self, array1, array2)
+    class(TestInstance), intent(in) :: self
+    class(*), intent(in) :: array1(:)
+    class(*), intent(in) :: array2(:)
+
+    real(kind=8), allocatable :: real_array_1(:)
+    real(kind=8), allocatable :: real_array_2(:)
+
+    select type (array1)
+      type is (real(kind=4))
+        select type (array2)
+          type is (real(kind=4))
+            call compare_arrays_r_4_4(self, array1, array2)
+          type is (real(kind=8))
+            call compare_arrays_r_4_8(self, array1, array2)
+        end select
+      type is (real(kind=8))
+        select type (array2)
+          type is (real(kind=4))
+            call compare_arrays_r_8_4(self, array1, array2)
+          type is (real(kind=8))
+            call compare_arrays_r_8_8(self, array1, array2)
+        end select
+    end select
+  end subroutine test__compare_arrays
+
+  subroutine compare_arrays_r_8_4(test, array1, array2)
+    class(TestInstance), intent(in) :: test
     real(kind=8), intent(in) :: array1(:)
     real(kind=4), intent(in) :: array2(:)
 
     if (size(array1) /= size(array2)) then
-      call bort("Array sizes mismatch.")
+      call test%failed("Array sizes mismatch.")
     end if
 
     if (any(abs(array1 - array2) > 1e-6)) then
-      call bort("Value mismatch.")
+      call test%failed("Value mismatch.")
     end if
   end subroutine compare_arrays_r_8_4
 
 
-  subroutine compare_arrays_r_4_4(array1, array2)
+  subroutine compare_arrays_r_4_4(test, array1, array2)
+    class(TestInstance), intent(in) :: test
     real(kind=4), intent(in) :: array1(:)
     real(kind=4), intent(in) :: array2(:)
 
     if (size(array1) /= size(array2)) then
-      call bort("Array sizes mismatch.")
+      call test%failed("Array sizes mismatch.")
     end if
 
     if (any(abs(array1 - array2) > 1e-6)) then
-      call bort("Value mismatch.")
+      call test%failed("Value mismatch.")
     end if
   end subroutine compare_arrays_r_4_4
 
 
-  subroutine compare_arrays_r_4_8(array1, array2)
+  subroutine compare_arrays_r_4_8(test, array1, array2)
+    class(TestInstance), intent(in) :: test
     real(kind=4), intent(in) :: array1(:)
     real(kind=8), intent(in) :: array2(:)
 
     if (size(array1) /= size(array2)) then
-      call bort("Array sizes mismatch.")
+      call test%failed("Array sizes mismatch.")
     end if
 
     if (any(abs(array1 - array2) > 1e-6)) then
-      call bort("Value mismatch.")
+      call test%failed("Value mismatch.")
     end if
   end subroutine compare_arrays_r_4_8
 
 
-  subroutine compare_arrays_r_8_8(array1, array2)
+  subroutine compare_arrays_r_8_8(test, array1, array2)
+    class(TestInstance), intent(in) :: test
     real(kind=8), intent(in) :: array1(:)
     real(kind=8), intent(in) :: array2(:)
 
     if (size(array1) /= size(array2)) then
-      call bort("Array sizes mismatch.")
+      call test%failed("Array sizes mismatch.")
     end if
 
     if (any(abs(array1 - array2) > 1e-6)) then
-      call bort("Value mismatch.")
+      call test%failed("Value mismatch.")
     end if
   end subroutine compare_arrays_r_8_8
 
