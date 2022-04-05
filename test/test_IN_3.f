5edb903e666d6ca52d9f9388ff7b72999d2fe112
Jeff Ator <Jeff.Ator@noaa.gov>
2022-04-05 18:39:46 +0000

    fix print messages to align with others

--- test/test_IN_3.f
+++ test/test_IN_3.f
@@ -168,13 +168,13 @@ C*	    Continue checking with the next subset.
                 IF ( IFBGET ( 11 ) .eq. 0 ) THEN
                     print *, '        IFBGET -> OK'
                 ELSE
-                    print *, '        IFBGET -> FAILED!'
+                    print *, '        IFBGET -> FAILED!!'
                 ENDIF
 
                 IF ( LCMGDF ( 11, cmgtag ) .eq. 0 ) THEN
                     print *, '        LCMGDF -> OK'
                 ELSE
-                    print *, '        LCMGDF -> FAILED!'
+                    print *, '        LCMGDF -> FAILED!!'
                 ENDIF
 
 		IF ( ( nr8rr .ne. 1 ) .or.
@@ -231,7 +231,7 @@ C*	    Continue checking with the next subset.
 	        IF ( IFBGET ( 11 ) .ne. 0 ) THEN
 	            print *, '        IFBGET -> OK'
 	        ELSE
-	            print *, '        IFBGET -> FAILED!'
+	            print *, '        IFBGET -> FAILED!!'
 	        ENDIF
 	    ENDIF
 
