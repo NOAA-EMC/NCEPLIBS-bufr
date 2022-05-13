e3968a6825c6c7a08085130d5d063a262fd26aed
Jack Woollen <Jack.Woollen@noaa.gov>
2022-05-13 21:28:31 +0000

    modify 8 byte packers to work with bufrlib_8

--- src/atepak.f
+++ src/atepak.f
@@ -14,12 +14,16 @@
 
       equivalence (nval8,nvals)
 
-      if(nbits<0      ) call bort('bufrlib: pkb8 - nbits < zero !!!!!') 
-      if(nbits>2*nbitw) call bort('bufrlib: pkb8 - nbits > 2*nbitw !!') 
+      if(nbits<0 ) call bort('bufrlib: pkb8 - nbits < zero !!!!!') 
+      if(nbits>64) call bort('bufrlib: pkb8 - nbits > 64   !!!!!') 
 
-      nval8=nval    
-      nval4=nvals(2); call pkb(nval4,max(nbits-nbitw,0),ibay,ibit)
-      nval4=nvals(1); call pkb(nval4,min(nbits,nbitw  ),ibay,ibit)
+      if(nbitw==32) then
+         nval8=nval    
+         nval4=nvals(2); call pkb(nval4,max(nbits-nbitw,0),ibay,ibit)
+         nval4=nvals(1); call pkb(nval4,min(nbits,nbitw  ),ibay,ibit)
+      else
+         call pkb(nval,nbits,ibay,ibit)
+      endif
 
       end subroutine 
 
@@ -52,17 +56,20 @@
       integer(4) :: nval2
       integer(8) :: nval3
 
-      if(nbits<0) call bort('BUFRLIB: UPB8 - nbits < zero !!!!!') 
-      if(nbits>2*nbitw) nval=0
-      if(nbits>2*nbitw) return
+      if(nbits<0 ) call bort('BUFRLIB: UPB8 - nbits < zero !!!!!') 
+      if(nbits>64) nval=0
+      if(nbits>64) return
 
-      jbit=ibit
-      call upb(nval1,max(nbits-nbitw,0),ibay,jbit)
-      call upb(nval2,min(nbitw,nbits  ),ibay,jbit)
-
-      nval3 = nval2
-      nval8 = nval1
-      nval8 = ior(nval8,nval3)
+      if(nbitw==32) then
+         jbit=ibit
+         call upb(nval1,max(nbits-nbitw,0),ibay,jbit)
+         call upb(nval2,min(nbitw,nbits  ),ibay,jbit)
+         nval3 = nval2
+         nval8 = nval1
+         nval8 = ior(nval8,nval3)
+      else
+         call upbb(nval8,nbits,ibit,ibay)
+      endif
 
       end subroutine 
 
