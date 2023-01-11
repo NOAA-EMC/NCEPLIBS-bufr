840d3d807ecc2a39f41dc2f4f03ce1a471703770
Edward Hartnett <edward.hartnett@noaa.gov>
2023-01-11 06:58:02 +0100

    doxygen changes

--- src/bufr_interface.h
+++ src/bufr_interface.h
@@ -21,8 +21,8 @@ extern "C" {
    Wraps fortran "open" statement so we can open a Fortran file
    from a C program.
 
-   @param unit - int: the integer to use as the fortran file unit
-   @param filepath - const char*: path to the file we want to open.
+   @param unit the integer to use as the fortran file unit.
+   @param filepath path to the file we want to open.
    
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -33,7 +33,7 @@ extern "C" {
    Wraps fortran "close" statement so we can close a Fortran file
    from a C program.
 
-   @param[in] unit - int: the integer to use as the fortran file unit
+   @param unit the integer to use as the fortran file unit.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -43,9 +43,9 @@ extern "C" {
 /**
    Wraps BUFRLIB "openbf" subroutine.
 
-   @param[in] bufr_unit - int: the fortran file unit number
-   @param[in] cio - const char*: cio string (ex "IN", "SEC3", and "OUT")
-   @param[in] table_file_id - int: table_file unit number
+   @param bufr_unit the fortran file unit number.
+   @param cio cio string (ex "IN", "SEC3", and "OUT").
+   @param table_file_id table_file unit number.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -55,7 +55,7 @@ extern "C" {
 /**
    Wraps BUFRLIB "closbf" subroutine.
 
-   @param[in] bufr_unit - int: the fortran file unit number to close
+   @param bufr_unit the fortran file unit number to close.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -74,10 +74,10 @@ extern "C" {
 /**
    Wraps BUFRLIB "ireadmg" subroutine.
 
-   @param[in] bufr_unit - int: the fortran file unit number to read from
-   @param[out] subset - char*: the subset string
-   @param[out] iddate - int*: datetime of message
-   @param[in] subset_len - int: length of the subset string
+   @param bufr_unit the fortran file unit number to read from.
+   @param subset the subset string.
+   @param iddate datetime of message.
+   @param subset_len length of the subset string.
 
    @return ???
 
@@ -89,7 +89,7 @@ extern "C" {
 /**
    Wraps BUFRLIB "ireadsb" function.
 
-   @param[in] bufr_unit - int: the fortran file unit number to read from
+   @param bufr_unit the fortran file unit number to read from.
 
    @return ???
 
@@ -101,11 +101,12 @@ extern "C" {
 /**
    Wraps BUFRLIB "ufbint" function.
 
-   @param[in] bufr_unit - int: the fortran file unit number to read from
-   @param[inout] c_data - void**: c style pointer to a pre-allocated buffer
-   @param[in] dim_1, dim_2 - int: dimensionality of data to read or write
-   @param[out] iret -  int: return value, length of data read
-   @param[in] table_b_mnemonic - const char*: string of mnemonics
+   @param bufr_unit the fortran file unit number to read from.
+   @param c_data pointer to a pointer to a pre-allocated buffer.
+   @param dim_1 dimensionality of data to read or write.
+   @param dim_2 dimensionality of data to read or write.
+   @param iret return value, length of data read.
+   @param table_b_mnemonic string of mnemonics.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -116,11 +117,12 @@ extern "C" {
 /**
    Wraps BUFRLIB "ufbrep" function.
 
-   @param[in] bufr_unit - int: the fortran file unit number to read from
-   @param[inout] c_data - void**: c style pointer to a pre-allocated buffer
-   @param[in] dim_1, dim_2 - int: dimensionality of data to read or write
-   @param[out] iret -  int: return value, length of data read
-   @param[in] table_b_mnemonic - const char*: string of mnemonics
+   @param bufr_unit the fortran file unit number to read from.
+   @param c_data pointer to a pointer to a pre-allocated buffer.
+   @param dim_1 dimensionality of data to read or write.
+   @param dim_2 dimensionality of data to read or write.
+   @param iret length of data read.
+   @param table_b_mnemonic string of mnemonics.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -131,9 +133,9 @@ extern "C" {
 /**
    Wraps BUFRLIB "mtinfo" function.
 
-   @param[in] path - const char*: the path where the WMO tables are stored
-   @param[in] file_unit_1 - int: number to use for first file unit
-   @param[in] file_unit_2 - int: number to use for second file unit
+   @param path the path where the WMO tables are stored.
+   @param file_unit_1 number to use for first file unit.
+   @param file_unit_2 number to use for second file unit.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -144,10 +146,10 @@ extern "C" {
 /**
    Wraps BUFRLIB "status" function.
 
-   @param[in] file_unit - int: the fortran file unit number to read from
-   @param[out] lun - int: pointer for the file stream
-   @param[out] il - int: file status
-   @param[out] im - int: message status
+   @param file_unit the fortran file unit number to read from.
+   @param lun pointer for the file stream.
+   @param il file status.
+   @param im message status.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -157,13 +159,13 @@ extern "C" {
 /**
    Gets Table B Unit and Description strings for a mnemonic. Wraps BUFRLIB "nemdefs".
 
-   @param[in] file_unit - int: Fortran file unit for the open file
-   @param[in] mnemonic - const char*: mnemonic
-   @param[out] unit_c - char*: unit str
-   @param[in] unit_str_len - int: unit str length
-   @param[out] desc_c - char*: description string
-   @param[in] desc_str_len - int: description str length
-   @param[out] iret - int*: return value. 0 indicates success -1 indicates failure.
+   @param file_unit Fortran file unit for the open file.
+   @param mnemonic mnemonic.
+   @param unit_c unit str.
+   @param unit_str_len unit str length.
+   @param desc_c description string.
+   @param desc_str_len description str length.
+   @param iret 0 indicates success -1 indicates failure.
 
    @author Ronald Mclaren @date 2020-07-29
 */
@@ -179,13 +181,13 @@ extern "C" {
 /**
    Gets Table B scale, reference, and bits values. Wraps BUFRLIB "nemspecs".
 
-   @param[in] file_unit - int: Fortran file unit for the open file
-   @param[in] mnemonic - const char*: mnemonic
-   @param[in] mnemonic_idx - int: indicates specific mnemonic element (if repeated)
-   @param[out] scale - int*: scale of element
-   @param[out] reference - int*: reference of element
-   @param[out] bits - int*: number of bits representing the element
-   @param[out] iret - int*: return value. 0 indicates success -1 indicates failure.
+   @param file_unit Fortran file unit for the open file.
+   @param mnemonic: mnemonic.
+   @param mnemonic_idx indicates specific mnemonic element (if repeated).
+   @param scale scale of element.
+   @param reference reference of element.
+   @param bits number of bits representing the element.
+   @param iret 0 indicates success -1 indicates failure.
    
    @author Ronald Mclaren @date 2022-08-08
 */
@@ -203,11 +205,11 @@ extern "C" {
    internal DX BUFR tables, based on the mnemonic associated with that
    descriptor.
    
-   @param[in] bufr_unit - int: the bufr file pointer
-   @param[in] mnemonic - const char*: mnemonic
-   @param[out] descriptor - int*: the binary descriptor for the mnemonic
-   @param[out] table_type char*: 'A', 'B', 'C', or 'D', depending on table type
-   @param[out] table_idx - int*: the table index, or 0 if not found
+   @param bufr_unit the bufr file pointer.
+   @param mnemonic mnemonic.
+   @param descriptor the binary descriptor for the mnemonic.
+   @param table_type 'A', 'B', 'C', or 'D', depending on table type.
+   @param table_idx the table index, or 0 if not found.
    
    @author Ronald Mclaren @date 2022-08-16
 */
@@ -221,13 +223,13 @@ extern "C" {
 /**  
      Get information about a Table B descriptor.
      
-     @param[in] bufr_unit - int: the bufr file pointer
-     @param[in] table_idx - int: Table B index
-     @param[out] unit_str - char*: unit str
-     @param[in] unit_str_len - int: unit str length
-     @param[out] scale - int*: scale of element
-     @param[out] reference - int*: reference of element
-     @param[out] bits - int*: bits of element
+     @param bufr_unit the bufr file pointer.
+     @param table_idx Table B index.
+     @param unit_str unit str.
+     @param unit_str_len unit str length.
+     @param scale scale of element.
+     @param reference reference of element.
+     @param bits bits of element.
      
      @author Ronald McLaren @date 2022-08-16
      
@@ -244,8 +246,8 @@ extern "C" {
 /**
    Get copy of the moda_tables ISC array.
    
-   @param[out] isc_ptr - int**: c style pointer to the ISC array
-   @param[out] isc_size - int*: size of the ISC array
+   @param isc_ptr pointer to a pointer to the ISC array.
+   @param isc_size size of the ISC array.
    
    @author Ronald McLaren @date 2022-03-23    
 */
@@ -255,8 +257,8 @@ extern "C" {
 /** 
     Get copy of the moda_tables LINK array.
     
-    @param[out] link_ptr - int**: c style pointer to the LINK array
-    @param[out] link_size - int*: size of the LINK array
+    @param link_ptr pointer to a pointer to the LINK array.
+    @param link_size size of the LINK array.
     
     @author Ronald McLaren @date 2022-03-23    
 */
@@ -266,8 +268,8 @@ extern "C" {
 /** 
     Get copy of the moda_tables ITP array.
 
-    @param[out] itp_ptr - int**: c style pointer to the ITP array
-    @param[out] itp_size - int*: size of the ITP array
+    @param itp_ptr pointer to a pointer to the ITP array.
+    @param itp_size size of the ITP array.
     
     @author Ronald McLaren @date 2022-03-23
     
@@ -278,9 +280,9 @@ extern "C" {
 /** 
     Get copy of the moda_tables TYP array.
 
-    @param[out] typ_ptr - char**: c style pointer to the TYP array
-    @param[out] typ_len - int*: size of each string within the TYP array
-    @param[out] mem_size - int*: size of the TYP array
+    @param typ_ptr pointer to a pointer to the TYP array.
+    @param typ_len size of each string within the TYP array.
+    @param mem_size size of the TYP array.
     
     @author Ronald McLaren @date 2022-03-23
 */
@@ -290,9 +292,9 @@ extern "C" {
 /** 
     Get copy of the moda_tables TAG array.
 
-    @param[out] tag_ptr - char**: c style pointer to the TAG array
-    @param[out] tag_len - int*: size of each string within the TAG array
-    @param[out] mem_size - int*: size of the TAG array
+    @param tag_ptr pointer to a pointer to the TAG array.
+    @param tag_len size of each string within the TAG array.
+    @param mem_size size of the TAG array.
     
     @author Ronald McLaren @date 2022-03-23
   
@@ -303,8 +305,8 @@ extern "C" {
 /** 
     Get copy of the moda_tables JMPB array.
 
-    @param[out] jmpb_ptr - int**: c style pointer to the JMPB array
-    @param[out] jmpb_size - int: size of the JMPB array
+    @param jmpb_ptr pointer to a pointer to the JMPB array.
+    @param jmpb_size size of the JMPB array.
     
     @author Ronald McLaren @date 2022-03-23
 */
@@ -315,8 +317,8 @@ extern "C" {
 /** 
     Get the bufr node idx for the start node of the subset.
 
-    @param[in] lun - int: pointer for the file stream
-    @param[out] start_node - int*: the start node of the subset
+    @param lun pointer for the file stream.
+    @param start_node the start node of the subset.
 
     @author Ronald McLaren @date 2022-03-23
 */
@@ -326,8 +328,8 @@ extern "C" {
 /** 
     Get the number of values in the current subset
 
-    @param[in] lun - int: pointer for the file stream
-    @param[out] num_nodes - int*: number of values in the subset
+    @param lun pointer for the file stream.
+    @param num_nodes number of values in the subset.
 
     @author Ronald McLaren @date 2022-03-23
 */
@@ -337,9 +339,9 @@ extern "C" {
 /** 
     Get pointer to the moda_usrint VAL array.
 
-    @param[in] lun - int: pointer for the file stream
-    @param[out] val_ptr - double**: c style pointer to the VAL array
-    @param[out] val_size - int*: size of the VAL array
+    @param lun pointer for the file stream.
+    @param val_ptr pointer to a pointer to the VAL array.
+    @param val_size size of the VAL array.
 
     @author Ronald McLaren @date 2022-03-23
 */
@@ -349,9 +351,9 @@ extern "C" {
 /** 
     Get pointer to the moda_usrint INV array.
 
-    @param[in] lun - int: pointer for the file stream
-    @param[out] inv_ptr - int**: c style pointer to the INV array
-    @param[out] inv_size - int*: size of the INV array
+    @param lun pointer for the file stream.
+    @param inv_ptr pointer to a pointer to the INV array.
+    @param inv_size size of the INV array.
     
     @author Ronald McLaren @date 2022-03-23
 */
