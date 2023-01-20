440c242edb72accef4243b37d7e2a67113299aab
Edward Hartnett <edward.hartnett@noaa.gov>
2023-01-20 09:44:52 +0100

    whitespace formatted sinv.f90.in and cmpbqm.f90 to get doxygen build to work

--- utils/cmpbqm.f90
+++ utils/cmpbqm.f90
@@ -9,115 +9,115 @@
 !> @return 0 for success, error code otherwise.
 !>
 !> @author J Woollen
-      PROGRAM CMPBQM
+PROGRAM CMPBQM
 
-      CHARACTER*255 FILE
-      CHARACTER*50 HEADR,OBSTR,QMSTR,FCSTR,ERSTR,QMSFC
-      CHARACTER*20 VARS(7)
-      CHARACTER*8  SUBSET,DATE
-      DIMENSION    KNT(300,7,0:17),HDR(5),OBS(8,255),QMS(8,255)
-      LOGICAL      SKIP,exist
-      REAL*8       HDR,OBS,QMS
+  CHARACTER*255 FILE
+  CHARACTER*50 HEADR,OBSTR,QMSTR,FCSTR,ERSTR,QMSFC
+  CHARACTER*20 VARS(7)
+  CHARACTER*8  SUBSET,DATE
+  DIMENSION    KNT(300,7,0:17),HDR(5),OBS(8,255),QMS(8,255)
+  LOGICAL      SKIP,exist
+  REAL*8       HDR,OBS,QMS
 
-      DATA HEADR /'SID XOB YOB DHR TYP              '/
-      DATA OBSTR /'POB QOB TOB ZOB UOB PWO RHO VOB  '/
-      DATA QMSTR /'PQM QQM TQM ZQM WQM PWQ RHQ      '/
+  DATA HEADR /'SID XOB YOB DHR TYP              '/
+  DATA OBSTR /'POB QOB TOB ZOB UOB PWO RHO VOB  '/
+  DATA QMSTR /'PQM QQM TQM ZQM WQM PWQ RHQ      '/
 
-      DATA VARS   /'PRESSURE        ',&
-                   'SPECIFIC HUMIDTY',&
-                   'TEMPERATURE     ',&
-                   'HEIGHT          ',&
-                   'WIND COMPONENTS ',&
-                   'PRECIPITABLE H2O',&
-                   'RELATIVE HUMIDTY'/
+  DATA VARS   /'PRESSURE        ',&
+       'SPECIFIC HUMIDTY',&
+       'TEMPERATURE     ',&
+       'HEIGHT          ',&
+       'WIND COMPONENTS ',&
+       'PRECIPITABLE H2O',&
+       'RELATIVE HUMIDTY'/
 
-      DATA LUBFR /8    /
-      DATA VMAX  /10E10/
+  DATA LUBFR /8    /
+  DATA VMAX  /10E10/
 
-!-----------------------------------------------------------------------
-!-----------------------------------------------------------------------
+  !-----------------------------------------------------------------------
+  !-----------------------------------------------------------------------
 
-      IREC = 0
-      KNT = 0
+  IREC = 0
+  KNT = 0
 
-!  OPEN A FILE - GET A DATE
-!  ------------------------
+  !  OPEN A FILE - GET A DATE
+  !  ------------------------
 
-      call getarg(1,file); file=trim(adjustl(file))
-      if (file == '') call bort('Usage: "cmpbqm prepbufrfile" will print prep inventory by variable, report type, and qc mark')
-      inquire(file=file,exist=exist)
-      if (.not.exist) call bort(trim(file)//' does not exist')
+  call getarg(1,file); file=trim(adjustl(file))
+  if (file == '') call bort('Usage: "cmpbqm prepbufrfile" will print prep inventory by variable, report type, and qc mark')
+  inquire(file=file,exist=exist)
+  if (.not.exist) call bort(trim(file)//' does not exist')
 
-      open(lubfr,file=file,form='unformatted')
-      CALL OPENBF(LUBFR,'IN',LUBFR)
-      CALL READMG(LUBFR,SUBSET,IDATE,IRET)
-      IF(IRET.NE.0) GOTO 900
-      WRITE(DATE,'(I8)') IDATE
-      DO I=1,8
-      IF(DATE(I:I).EQ.' ') DATE(I:I) = '0'
-      ENDDO
-      PRINT'(''DATA  VALID AT  '',A8)',DATE
+  open(lubfr,file=file,form='unformatted')
+  CALL OPENBF(LUBFR,'IN',LUBFR)
+  CALL READMG(LUBFR,SUBSET,IDATE,IRET)
+  IF(IRET.NE.0) GOTO 900
+  WRITE(DATE,'(I8)') IDATE
+  DO I=1,8
+     IF(DATE(I:I).EQ.' ') DATE(I:I) = '0'
+  ENDDO
+  PRINT'(''DATA  VALID AT  '',A8)',DATE
 
-!  READ THRU THE PREPDA RECORDS
-!  ----------------------------
+  !  READ THRU THE PREPDA RECORDS
+  !  ----------------------------
 
-10    CALL READSB(LUBFR,IRET)
-      IF(IRET.NE.0) THEN
-         CALL READMG(LUBFR,SUBSET,IDATE,IRET)
-         IF(IRET.NE.0) GOTO 100
-         CALL UFBCNT(LUBFR,IREC,ISUB)
-         GOTO 10
-      ENDIF
-      QMS = 10E10
-      CALL UFBINT(LUBFR,HDR,5,1,IRET,HEADR)
-      CALL UFBINT(LUBFR,OBS,8,255,NLEV,OBSTR)
-      CALL UFBINT(LUBFR,QMS,8,255,NLEV,QMSTR)
+10 CALL READSB(LUBFR,IRET)
+  IF(IRET.NE.0) THEN
+     CALL READMG(LUBFR,SUBSET,IDATE,IRET)
+     IF(IRET.NE.0) GOTO 100
+     CALL UFBCNT(LUBFR,IREC,ISUB)
+     GOTO 10
+  ENDIF
+  QMS = 10E10
+  CALL UFBINT(LUBFR,HDR,5,1,IRET,HEADR)
+  CALL UFBINT(LUBFR,OBS,8,255,NLEV,OBSTR)
+  CALL UFBINT(LUBFR,QMS,8,255,NLEV,QMSTR)
 
-      KX = HDR(5)
+  KX = HDR(5)
 
-      DO L=1,NLEV
-      DO K=1,7
-      IQ = -1
-      IF(K.EQ.5) OBS(5,L) = MAX(OBS(5,L),OBS(8,L))
-      IF(OBS(K,L).LT.VMAX .AND. QMS(K,L).LT.VMAX) THEN
-         IQ = QMS(K,L)
-      ELSEIF(OBS(K,L).LT.VMAX .AND. QMS(K,L).GE.VMAX) THEN
-         IQ = 16
-      ELSEIF(OBS(K,L).GE.VMAX .AND. QMS(K,L).LT.VMAX) THEN
-         IQ = 17
-      ENDIF
-      IF(IQ.GE.0) KNT(KX,K,IQ) = KNT(KX,K,IQ)+1
-      ENDDO
-      ENDDO
+  DO L=1,NLEV
+     DO K=1,7
+        IQ = -1
+        IF(K.EQ.5) OBS(5,L) = MAX(OBS(5,L),OBS(8,L))
+        IF(OBS(K,L).LT.VMAX .AND. QMS(K,L).LT.VMAX) THEN
+           IQ = QMS(K,L)
+        ELSEIF(OBS(K,L).LT.VMAX .AND. QMS(K,L).GE.VMAX) THEN
+           IQ = 16
+        ELSEIF(OBS(K,L).GE.VMAX .AND. QMS(K,L).LT.VMAX) THEN
+           IQ = 17
+        ENDIF
+        IF(IQ.GE.0) KNT(KX,K,IQ) = KNT(KX,K,IQ)+1
+     ENDDO
+  ENDDO
 
-      GOTO 10
+  GOTO 10
 
-!  FINISH UP
-!  ---------
+  !  FINISH UP
+  !  ---------
 
-100   DO K=1,7
-      PRINT*,VARS(K)
-      PRINT*
-      DO KX=1,300
-      ITOT = 0; igood=0; ifail=0
-      DO IQ=0,17
-      ITOT = ITOT+KNT(KX,K,IQ)
-      if(iq.le.3) then
-         igood=igood+KNT(KX,K,IQ)
-      elseif(iq.le.7) then
-         ifail=ifail+KNT(KX,K,IQ)
-      endif
-      ENDDO
-      IF(ITOT.GT.0) PRINT 101,KX,ITOT,igood,ifail,(KNT(KX,K,IQ),IQ=8,17)
-101   FORMAT(I3,I6,2('|', I6),&
-                   2('|', I6),&
-                   1('|',6I6),&
-                   2('|', I6))
-      ENDDO
-      PRINT*
-      ENDDO
+100 DO K=1,7
+     PRINT*,VARS(K)
+     PRINT*
+     DO KX=1,300
+        ITOT = 0; igood=0; ifail=0
+        DO IQ=0,17
+           ITOT = ITOT+KNT(KX,K,IQ)
+           if(iq.le.3) then
+              igood=igood+KNT(KX,K,IQ)
+           elseif(iq.le.7) then
+              ifail=ifail+KNT(KX,K,IQ)
+           endif
+        ENDDO
+        IF(ITOT.GT.0) PRINT 101,KX,ITOT,igood,ifail,(KNT(KX,K,IQ),IQ=8,17)
+101     FORMAT(I3,I6,2('|', I6),&
+             2('|', I6),&
+             1('|',6I6),&
+             2('|', I6))
+     ENDDO
+     PRINT*
+  ENDDO
 
-      PRINT*,'******CMPBQM PROCESSED ',IREC,' BUFR RECORDS******'
-      STOP
-900   CALL BORT('CMPBQM - ERROR READING BUFR FILE ')
-      END PROGRAM
+  PRINT*,'******CMPBQM PROCESSED ',IREC,' BUFR RECORDS******'
+  STOP
+900 CALL BORT('CMPBQM - ERROR READING BUFR FILE ')
+END PROGRAM CMPBQM
