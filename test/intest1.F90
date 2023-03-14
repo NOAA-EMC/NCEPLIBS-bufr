749bdcab03698be76d036c314c1fbe6272482c9b
Alex Richert <alexander.richert@noaa.gov>
2023-03-14 12:44:20 -0700

    Update comment

--- test/intest1.F90
+++ test/intest1.F90
@@ -101,7 +101,7 @@ program intest1
   call readlc(11, softvstg, 'SOFTV')
   IF (softvstg(5:12) .ne. '5.8.5.10') stop 17
 
-  ! Get section length
+  ! Read and check section lengths
   call getlens(ibfmg,5,l0,l1,l2,l3,l4,l5)
   if (.not. all((/l0,l1,l2,l3,l4,l5/) .eq. (/8,22,0,24,4111,4/))) stop 18
 
