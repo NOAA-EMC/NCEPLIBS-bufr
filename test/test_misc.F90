fe82e297f01a4a1ac96a7eac34feeaeb6c19fd7a
Edward Hartnett <edward.hartnett@noaa.gov>
2023-03-18 09:30:40 -0600

    more testing

--- test/test_misc.F90
+++ test/test_misc.F90
@@ -9,12 +9,45 @@ program test_bort
   character*5 char5
   character*5 adn30
   integer a, idn30
+  integer lun, il, im
+  integer ios
 
   print *, 'Testing misc subroutines.'
+
+#ifdef KIND_8
+  call setim8b(.true.)
+#endif
+
+  ! adn30/idn30.
   char5 = adn30(42, 5)
   if (char5 .ne. '00042') stop 1
   a = idn30(char5, 5)
   if (a .ne. 42) stop 2
-  print *, 'SUCCESS'
+
+  ! testiing status()
+  open(unit = 11, file = 'testfiles/IN_2', form = 'UNFORMATTED', iostat = ios)
+  if (ios .ne. 0) stop 3
+  call openbf(11, 'IN', 11)
+  call status(11, lun, il, im)
+  if (lun .ne. 1 .or. il .ne. -1 .or. im .ne. 0) stop 4
+  call closbf(11)
+
+  ! Try again.
+  open(unit = 11, file = 'testfiles/IN_2', form = 'UNFORMATTED', iostat = ios)
+  if (ios .ne. 0) stop 3
+  call openbf(11, 'IN', 11)
+  call status(11, lun, il, im)
+  if (lun .ne. 1 .or. il .ne. -1 .or. im .ne. 0) stop 4
+  call closbf(11)
+
+  ! ! Open for OUT.
+  ! open(unit = 11, file = 'testfiles/IN_2', form = 'UNFORMATTED', iostat = ios)
+  ! if (ios .ne. 0) stop 3
+  ! call openbf(11, 'OUT', 11)
+  ! call status(11, lun, il, im)
+  ! print *, lun, il, im
+  ! if (lun .ne. 1 .or. il .ne. -1 .or. im .ne. 0) stop 4
+  ! call closbf(11)
   
+  print *, 'SUCCESS'
 end program test_bort
