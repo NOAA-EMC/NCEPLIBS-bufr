edfabafe2ce16a42e1571d76564ed39f755a5826
Jeff Ator <Jeff.Ator@noaa.gov>
2023-03-22 17:07:34 +0000

    simplify strnum code and testing

--- src/strnum.F90
+++ src/strnum.F90
@@ -8,8 +8,8 @@
 !> contain only digits and an (optional) leading sign ('+' or '-')
 !> character. 
 !>
-!> If the string contains all blank characters, then num is returned
-!> with a value of 0.
+!> If the string is empty or contains all blank characters, then num
+!> is returned with a value of 0.
 !>
 !> @param[in]  str -- character*(*): String
 !> @param[out] num -- integer: Value decoded from str
@@ -18,7 +18,7 @@
 !>  -1 = string contained one or more illegal characters
 !>
 !> @author J. Woollen @date 1994-01-06
-recursive subroutine strnum(str,num,iret)
+recursive subroutine strnum( str, num, iret )
   use modv_im8b
 
   implicit none
@@ -29,7 +29,7 @@ recursive subroutine strnum(str,num,iret)
 
   character str2*40, fmt*8
 
-  integer lens
+  integer lens, ios
 
 !-----------------------------------------------------------------------
 !-----------------------------------------------------------------------
@@ -47,27 +47,17 @@ recursive subroutine strnum(str,num,iret)
      return
   end if
 
-  iret = 0
-
-! Check for blank input string.
-
-  if ( str .eq. ' ' ) then
-    num = 0
-    return
-  end if
-
-! Verify that the string contains all legal characters.
-
-  call strsuc ( str, str2, lens )
-  if ( verify ( str2(1:lens), "0123456789+-" ) .ne. 0 ) then
-    iret = -1
-    return
-  end if
-
 ! Decode the integer from the string.
 
+  iret = 0
+  num = 0
+
+  call strsuc ( str, str2, lens )
+  if ( lens .eq. 0 ) return
   write ( fmt, '(''(I'',I2,'')'')' ) lens
-  read ( str2(1:lens), fmt ) num
+
+  read ( str2(1:lens), fmt, iostat = ios ) num
+  if ( ios .ne. 0 ) iret = -1
 
   return
 end subroutine strnum
