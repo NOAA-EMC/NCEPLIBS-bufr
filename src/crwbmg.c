be3a08ab609da3e30ae03439e481d038c8f24400
Jeff Ator <Jeff.Ator@noaa.gov>
2023-03-24 17:59:05 +0000

    fix punctuation in MXFNLEN documentation to make Doxygen happy (take 4)

--- src/crwbmg.c
+++ src/crwbmg.c
@@ -5,6 +5,9 @@
  */
 #include "bufrlib.h"
 
+/** Maximum length of a system file, including any directory prefixes or other local filesystem notation. */
+#define MXFNLEN 200
+
 /** File pointers; each element will automatically initialize to NULL */
 FILE *pbf[2];
 
@@ -90,9 +93,6 @@ int rbytes( char *bmg, int mxmb, int isloc, int newbytes )
  */
 void cobfl( char *bfl, char io )
 {
-   /** Maximum length of a system file, including any directory prefixes or other local filesystem notation. */
-   #define MXFNLEN 200
-
    char lbf[MXFNLEN+1];
    char lio;
 
