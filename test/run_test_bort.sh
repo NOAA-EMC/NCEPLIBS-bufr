fbec941351f05a5b8910f9010e053cd0ab94be7e
Edward Hartnett <edward.hartnett@noaa.gov>
2023-04-13 05:38:30 -0600

    more borts

--- test/run_test_bort.sh
+++ test/run_test_bort.sh
@@ -75,6 +75,17 @@ for kind in "4" "d"; do
     (./test_bort_$kind ifbget 2) && exit 1
     (./test_bort_$kind ifbget 3) && exit 1
 
+    # Check isize().
+    (./test_bort_$kind isize 1) && exit 1
+    
+    # Check iupm().
+#    (./test_bort_$kind iupm 1) && exit 1
+    
+    # Check iupvs01().
+#    (./test_bort_$kind iupvs01 1) && exit 1
+#    (./test_bort_$kind iupvs01 2) && exit 1
+#    (./test_bort_$kind iupvs01 3) && exit 1
+    
     # Check nemtba().
     (./test_bort_$kind nemtba 1) && exit 1
 
