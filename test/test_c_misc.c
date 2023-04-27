c85f01d1530fe8cfa7dcd65de9e8c9e271d6cf80
Edward Hartnett <edward.hartnett@noaa.gov>
2023-04-27 11:35:46 -0600

    more C code tests

--- test/test_c_misc.c
+++ test/test_c_misc.c
@@ -7,14 +7,29 @@
 #include <stdio.h>
 #include "bufr_interface.h"
 
+#define TESTFILE_IN1 "testfiles/IN_1"
+
 int rbytes(char *bmg, int mxmb, int isloc, int newbytes);
 
 /* Test various C things. */
 int main()
 {
+    char bmg[200];
+    int nmb, iret;
+
+    printf("Testing misc C functions.\n");
     
     /* Test rbytes error condition. */
     if (rbytes(NULL, 0, 1, 1) != 1)
         return 1;
+
+    /* Test crbmg error condition. */
+    cobfl(TESTFILE_IN1, 'r');
+    crbmg(bmg, 1, &nmb, &iret);
+    if (iret != 1)
+        return 1;
+    ccbfl();
+    
+    printf("SUCCESS!!\n");
     return 0;
 }
