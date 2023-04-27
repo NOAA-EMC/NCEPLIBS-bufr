8b99cb4c4eebe0bad8988b617dc5abc23ce7e8f3
Edward Hartnett <edward.hartnett@noaa.gov>
2023-04-27 11:53:37 -0600

    adding test for stseq()

---
+++ test/test_c_stseq.c
@@ -0,0 +1,18 @@
+/* This is a test for NCEPLIBS-bufr.
+ *
+ * This file tests the stseq() function.
+ *
+ * Ed Hartnett 4/27/23 */
+
+#include <stdio.h>
+#include "bufr_interface.h"
+
+/* Test the stseq() function. */
+int main()
+{
+    printf("Testing the stseq() function.\n");
+
+    printf("SUCCESS!!\n");
+    
+    return 0;
+}
