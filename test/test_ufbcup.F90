c210937dee4ae37779f32c428fb87071ec63cd94
Edward Hartnett <edward.hartnett@noaa.gov>
2023-04-29 07:50:55 -0600

    more test development

--- test/test_ufbcup.F90
+++ test/test_ufbcup.F90
@@ -41,5 +41,25 @@ program test_ufbcup
   call closbf(11)
   call closbf(12)
 
+  ! Now reopen the file as input.
+  open(unit = 12, file = 'test_ufbcup_out', form ='unformatted')
+  call openbf(12, 'IN', 12)
+
+  ! Read a message in the file.
+  call readmg(12, subset, jdate, iret)
+  if (iret .ne. 0 .or. subset .ne. 'ADPSFC' .or. jdate .ne. 23022519) stop 10
+
+  ! Load a subset of data.
+!  call readsb(12, iret)
+!  if (iret .ne. 0) stop 30
+
+  ! Dump the data subset.
+!  open(unit = 13, file = 'test_ufbcup_out.txt')
+!  call ufdump(12, 13)
+!  close(13)
+
+  ! Close the file again.
+  call closbf(12)
+
   print *, 'SUCCESS'
 end program test_ufbcup
