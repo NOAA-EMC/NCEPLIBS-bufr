ab74b742ebbc46cced0e89361205977537a2b695
Edward Hartnett <edward.hartnett@noaa.gov>
2023-04-30 05:12:27 -0600

    more test development

--- test/test_ufbcup.F90
+++ test/test_ufbcup.F90
@@ -1,13 +1,13 @@
 ! This is a test for NCEPLIBS-bufr library.
 !
-! This tests random subroutines that needed some tests to complete
-! test code coverage.
+! This tests the ufbcub() subroutine.
 !
-! Ed Hartnett 3/17/23
+! Ed Hartnett 4/20/23
 program test_ufbcup
   implicit none
   character*8 subset
   integer jdate, iret
+  real*8 hdr(1, 1)
   
   print *, 'Testing ufbcup.'
 
@@ -37,17 +37,30 @@ program test_ufbcup
   ! Call ufbcup.
   call ufbcup(11, 12)
 
+  ! Write the data subset.
+  call writsb(12)
+
   ! Close files.
   call closbf(11)
   call closbf(12)
 
   ! Now reopen the file as input.
   open(unit = 12, file = 'test_ufbcup_out', form ='unformatted')
-  call openbf(12, 'IN', 12)
+
+  ! Open the file, and read the 1st subset of the 1st message.
+  call rdmgsb(12, 1, 1)
+
+  ! Get IREC, the number of BUFR messages, according to ufbint()
+  ! documentation. But I get a very large number of messages!
+  call ufbint(12, hdr, 1, 1, iret, 'IREC')
+  print *, hdr(1,1)
+  if (hdr(1,1) .ne. 100000000000.00000_8) stop 30
+  
+!  call openbf(12, 'IN', 12)
 
   ! Read a message in the file.
-  call readmg(12, subset, jdate, iret)
-  if (iret .ne. 0 .or. subset .ne. 'ADPSFC' .or. jdate .ne. 23022519) stop 10
+!  call readmg(12, subset, jdate, iret)
+!  if (iret .ne. 0 .or. subset .ne. 'ADPSFC' .or. jdate .ne. 23022519) stop 10
 
   ! Load a subset of data.
 !  call readsb(12, iret)
