c4aea4466984f58c925276a9d662d1da329f0513
Jeff Ator <Jeff.Ator@noaa.gov>
2024-05-09 20:10:31 +0000

    MacOS debugging (take 4)

--- utils/debufr.F90
+++ utils/debufr.F90
@@ -107,11 +107,10 @@ subroutine fdebufr_c ( ofile, lenof, tbldir, lentd, tblfil, lentf, prmstg, lenps
   prmstg_f = transfer ( prmstg(1:lenps), prmstg_f )
   print *, 'in debufr.F90, basic, forcemt, cfms passed from debufr.c = >', basic, '< >', forcemt, '< >', cfms, '<'
   basic_f = transfer ( basic, basic_f )
-  !basic_f = basic
   print *, 'in debufr.F90, initially set basic_f = >', basic_f, '<'
-  forcemt_f = forcemt
+  forcemt_f = transfer ( forcemt, forcemt_f )
   print *, 'in debufr.F90, initially set forcemt_f = >', forcemt_f, '<'
-  cfms_f = cfms
+  cfms_f = transfer ( cfms, cfms_f )
   print *, 'in debufr.F90, initially set cfms_f = >', cfms_f, '<'
 
   ! Open the output file.
